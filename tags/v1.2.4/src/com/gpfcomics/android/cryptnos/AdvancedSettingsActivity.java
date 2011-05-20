1e114b70cb05b28057092b8a8ecf25bc1db75dae
jeff.darlington <jeff.darlington@c3c0b870-714f-9574-d367-bfe6b2766405>
2011-05-20 19:26:54 +0000

    Creating official 1.2.4 tag

---
+++ tags/v1.2.4/src/com/gpfcomics/android/cryptnos/AdvancedSettingsActivity.java
@@ -0,0 +1,332 @@
+/* AdvancedSettingsActivity.java
+ * 
+ * PROGRAMMER:    Jeffrey T. Darlington
+ * DATE:          October 22, 2010
+ * PROJECT:       Cryptnos (Android)
+ * ANDROID V.:	  1.1
+ * REQUIRES:      
+ * REQUIRED BY:   
+ * 
+ * This activity allows the user to tweak some of the more advanced settings within
+ * Cryptnos, such as which text encoding to use.  Added in Cryptnos 1.2.0.
+ * 
+ * UPDATES FOR 1.2.1:  Minor UI enhancements
+ * 
+ * UPDATES FOR 1.2.4:  Added checkbox to manage the "copy to clipboard" setting
+ * 
+ * This program is Copyright 2011, Jeffrey T. Darlington.
+ * E-mail:  android_support@cryptnos.com
+ * Web:     http://www.cryptnos.com/
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See theGNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA.
+*/
+package com.gpfcomics.android.cryptnos;
+
+import java.nio.charset.Charset;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemSelectedListener;
+
+/**
+ * This activity allows the user to tweak some of the more advanced settings within
+ * Cryptnos, such as which text encoding to use.
+ * @author Jeffrey T. Darlington
+ * @version 1.2.4
+ * @since 1.2
+ */
+public class AdvancedSettingsActivity extends Activity {
+
+	/** A constant identifying the confirmation dialog displayed if the user
+	 *  upgrades changes the text encoding selection */
+	private static final int DIALOG_CONFIRM_ENCODING_CHANGE = 700;
+
+	/** A constant indicating the Help option menu item. */
+	private static final int OPTMENU_HELP = Menu.FIRST;
+
+	/** The Spinner holding our text encoding options */
+	private Spinner spinEncodings = null;
+	
+	/** A TextView to display the system default encoding, for reference */
+	private TextView labelDefaultEncoding = null;
+	
+	/** A spinner to show the available file manager list */
+	private Spinner spinFileManagers = null;
+	
+	/** A TextView prompt for the file manager spinner */
+	private TextView labelFileManagerPreference = null;
+	
+	/** A Text View displayed if no file managers were found */
+	private TextView labelNoFileManagersAvailable = null;
+	
+	/** A CheckBox to manage the "copy password to clipboard" setting */
+	private CheckBox chkCopyPasswordsToClipboard = null;
+	
+	/** A reference to the linear layout that contains our UI elements */
+	private LinearLayout layout = null;
+
+	/** A reference back to the main application */
+	private CryptnosApplication theApp = null;
+	
+	/** The SharedPreferences for Cryptnos, obtained from the
+	 *  CryptnosApplication.getPrefs() call.  We'll put this in its own
+	 *  variable for convenience. */
+	private SharedPreferences prefs = null;
+	
+	/** The current selection in the encoding spinner.  This is used to restore the
+	 *  previous selection if the user cancels the change. */
+	private int lastEncodingSelection = 0;
+	
+	/** The current selection in the file manager spinner.  This is used to restore
+	 *  the previous selection if the user cancels the change. */
+	private int lastFileManagerSelection = 0;
+	
+    @Override
+	public void onCreate(Bundle savedInstanceState) {
+        // The usual GUI setup stuff:
+    	super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_layout);
+        
+        // Get references to the app and preferences:
+        theApp = (CryptnosApplication)getApplication();
+        prefs = theApp.getPrefs();
+        
+        // Get a reference to our layout:
+        layout = (LinearLayout)findViewById(R.id.layoutSettings);
+        
+        // Get the current system default text encoding.  We'll use this to display
+        // the default in a label later.
+        String systemDefault = null;
+        try { systemDefault = System.getProperty("file.encoding", "Unknown"); }
+        catch (Exception ex) { systemDefault = "Unknown"; }
+        
+        // Get the current text encoding value from the preferences if possible,
+        // falling back to the system encoding or just plain UTF-8 if necessary:
+        String currentEncoding = null;
+        try {
+        	currentEncoding = prefs.getString(CryptnosApplication.PREFS_TEXT_ENCODING,
+        		System.getProperty("file.encoding",
+        				CryptnosApplication.TEXT_ENCODING_UTF8));
+        }
+        catch (Exception e) {
+        	currentEncoding = prefs.getString(CryptnosApplication.PREFS_TEXT_ENCODING,
+            		CryptnosApplication.TEXT_ENCODING_UTF8);
+        }
+        
+        // Set up the encodings spinner.  First we'll get a reference, then get a
+        // list of all encodings the system supports.  We'll find out where the
+        // current encoding is in the list, then create an adapter for the list,
+        // assign that to the spinner, and make the current encoding the selected
+        // value.
+        spinEncodings = (Spinner)findViewById(R.id.spinEncodings);
+        String[] charsets = Charset.availableCharsets().keySet().toArray(new String[1]);
+        for (int i = 0; i < charsets.length; i++) {
+        	if (charsets[i].compareTo(currentEncoding) == 0) {
+        		lastEncodingSelection = i;
+        		break;
+        	}
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+        		android.R.layout.simple_spinner_item, charsets);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinEncodings.setAdapter(adapter);
+        spinEncodings.setSelection(lastEncodingSelection, true);
+        spinEncodings.setPrompt(getResources().getString(R.string.settings_encoding_prompt));
+
+        // Set up the encoding spinner's selection listener.  If the user selects
+        // anything different from the current selection, show the confirmation
+        // dialog to make sure they want to make the change.
+        spinEncodings.setOnItemSelectedListener(new OnItemSelectedListener() {
+			public void onItemSelected(AdapterView<?> arg0, View arg1,
+					int arg2, long arg3) {
+				if (spinEncodings.getSelectedItemPosition() != lastEncodingSelection)
+					showDialog(DIALOG_CONFIRM_ENCODING_CHANGE);
+			}
+			public void onNothingSelected(AdapterView<?> arg0) { }
+        });
+        
+        // Set up the default encoding label:
+        labelDefaultEncoding = (TextView)findViewById(R.id.labelDefaultEncoding);
+        String labelText =
+        	getResources().getString(R.string.settings_default_encoding_label);
+        labelDefaultEncoding.setText(labelText.replace(getResources().getString(R.string.meta_replace_token),
+        		systemDefault));
+        
+        // Get handy references to the file manager UI elements:
+    	spinFileManagers = (Spinner)findViewById(R.id.spinFileManagers);
+    	labelFileManagerPreference = (TextView)findViewById(R.id.labelFileManagerPreference);
+    	labelNoFileManagersAvailable = (TextView)findViewById(R.id.labelNoFileManagersAvailable);
+
+    	// Get the app FileManager object and a list of available file managers.
+    	// If any file managers were found, set up the spinner to let the user
+    	// chose:
+        FileManager fm = theApp.getFileManager();
+        int[] availableFMs = fm.getAvailableFileManagers();
+        if (availableFMs != null && availableFMs.length > 0) {
+        	// Hide the "no file managers found" label:
+        	//labelNoFileManagersAvailable.setVisibility(View.INVISIBLE);
+        	layout.removeView(labelNoFileManagersAvailable);
+        	// Get the list of file manager names, as well as the currently
+        	// selected one by name:
+        	String[] fmList = fm.getAvailableFileManagerNames();
+        	String selected = fm.getPreferredFileManagerName();
+        	// Default to the first position in the list, which should be the
+        	// "no preference" option:
+        	lastFileManagerSelection = 0;
+        	// There should be a better way to do this, but step through the list
+        	// and compare the selected name with the names in the list.  If we
+        	// find it, take note of its position.  Fortunately, this list should
+        	// be small, so this should taken long.
+        	for (int i = 0; i < fmList.length; i++) {
+        		if (fmList[i].compareTo(selected) == 0) {
+        			lastFileManagerSelection = i;
+        			break;
+        		}
+        	}
+        	// Set up the adapter with the spinner:
+            ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this,
+            		android.R.layout.simple_spinner_item, fmList);
+    		adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinFileManagers.setAdapter(adapter2);
+            // Set the current selection and prompt:
+            spinFileManagers.setSelection(lastFileManagerSelection, true);
+            spinFileManagers.setPrompt(getResources().getString(R.string.settings_file_manager_prompt));
+
+        } else {
+        	//spinFileManagers.setVisibility(View.INVISIBLE);
+        	//labelFileManagerPreference.setVisibility(View.INVISIBLE);
+        	layout.removeView(spinFileManagers);
+        	layout.removeView(labelFileManagerPreference);
+        	labelNoFileManagersAvailable.setText(labelNoFileManagersAvailable.getText().toString() +
+        			fm.getRecognizedFileManagerNames());
+        }
+        
+        // Set the action when the file manager spinner changes.  Basically, we'll
+        // just set the new preference in the FileManager, which will handle the
+        // real work behind the scenes.
+        spinFileManagers.setOnItemSelectedListener(new OnItemSelectedListener() {
+			public void onItemSelected(AdapterView<?> arg0, View arg1,
+					int arg2, long arg3) {
+				if (spinFileManagers.getSelectedItemPosition() != lastFileManagerSelection) {
+					theApp.getFileManager().setPreferredFileManager((String)spinFileManagers.getSelectedItem());
+				}
+			}
+			public void onNothingSelected(AdapterView<?> arg0) { }
+        });
+        
+        // Get the checkbox to handle the "copy passwords to clipboard" setting
+        // and set it to the current state of the setting.  Then give the checkbox
+        // some functionality and let it toggle the setting in the preferences.
+        chkCopyPasswordsToClipboard =
+        	(CheckBox)findViewById(R.id.chkCopyPasswordsToClipboard);
+        chkCopyPasswordsToClipboard.setChecked(theApp.copyPasswordsToClipboard());
+        chkCopyPasswordsToClipboard.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				theApp.toggleCopyPasswordsToClipboard();
+			}
+        });
+        
+    }
+    
+    protected Dialog onCreateDialog(int id) {
+    	Dialog dialog = null;
+    	final Activity theActivity = this;
+    	switch (id)
+    	{
+    		// Set up the confirmation dialog for changing the encoding:
+			case DIALOG_CONFIRM_ENCODING_CHANGE:
+				AlertDialog.Builder adb = new AlertDialog.Builder(this);
+				dialog = (Dialog)adb.create();
+				adb.setMessage(getResources().getString(R.string.settings_confirm_encoding_change));
+				adb.setCancelable(true);
+				adb.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+					// If they said yes, change the preferences to use the new
+					// character encoding, refresh the parameter salt, and 
+					// take note of the new encoding position.
+					public void onClick(DialogInterface dialog, int which) {
+						try {
+							String newEncoding = (String)spinEncodings.getSelectedItem();
+							theApp.setTextEncoding(newEncoding);
+							theApp.refreshParameterSalt();
+							lastEncodingSelection = spinEncodings.getSelectedItemPosition();
+						}
+						catch (Exception ex) {
+							Toast.makeText(getBaseContext(), ex.getMessage(),
+									Toast.LENGTH_LONG).show();
+						}
+					}
+	    		});
+				// If they said no, simply cancel the dialog.  Canceling does the
+				// same as saying no, so we'll handle both cases below.
+    			adb.setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+ 		           public void onClick(DialogInterface dialog, int id) {
+ 		        	   dialog.cancel();
+ 		           }
+ 		       	});
+    			// If they cancel the dialog, dismiss it and reset the encoding
+    			// selection back to where it was before:
+    			adb.setOnCancelListener(new DialogInterface.OnCancelListener() {
+					public void onCancel(DialogInterface dialog) {
+						theActivity.dismissDialog(DIALOG_CONFIRM_ENCODING_CHANGE);
+						spinEncodings.setSelection(lastEncodingSelection, true);
+					}
+				});
+				dialog = (Dialog)adb.create();
+				break;
+    	}
+    	return dialog;
+    }
+    
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+    	// Add the "Help" menu items:
+    	menu.add(0, OPTMENU_HELP, Menu.NONE,
+        	R.string.optmenu_help).setIcon(android.R.drawable.ic_menu_help);
+    	return true;
+    }
+    
+	@Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	switch (item.getItemId()) {
+	    	// If the Help item is selected, open up the "Advanced Settings"
+    		// help.  (Should this open the help index, rather than this
+    		// specific item?  I'm not sure.  We can always change that, but
+    		// such a change will require using the HelpMenuActivity rather
+    		// than the HelpActivity.)
+	    	case OPTMENU_HELP:
+	        	Intent i = new Intent(this, HelpActivity.class);
+	        	i.putExtra("helptext", R.string.help_text_settings);
+	        	startActivity(i);
+	    		return true;
+    	}
+    	return false;
+    }
+	
+
+}
---
+++ tags/v1.2.4/src/com/gpfcomics/android/cryptnos/GenerateExistingActivity.java
@@ -0,0 +1,298 @@
+/* GenerateExistingActivity.java
+ * 
+ * PROGRAMMER:    Jeffrey T. Darlington
+ * DATE:          December 15, 2009
+ * PROJECT:       Cryptnos (Android)
+ * ANDROID V.:	  1.1
+ * REQUIRES:      
+ * REQUIRED BY:   
+ * 
+ * This activity provides a read-and-generate-only interface to the set of
+ * site parameters selected in the calling activity.  For users of the .NET
+ * version of Cryptnos, this is equivalent to the "Lock parameters" option;
+ * all the site parameters are displayed to the user as a read-only TextView,
+ * except for the passphrase, which must be entered by the user.  This
+ * provides an easy way to call up a set of parameters and generate the
+ * associated password without worrying about fat-fingering screwing up the
+ * database.
+ * 
+ * UPDATES FOR 1.2.0:  Added Help option menu
+ * 
+ * UPDATES FOR 1.2.2:  Added code to temporarily save and restore state if the
+ * user rotates the screen or slides out a physical keyboard.  Android handles
+ * these events by destroying the activity and rebuilding it, which results in
+ * the master and generated passwords being wiped out.  It should now hold onto
+ * these values temporarily and restore them once the rebuild is complete.
+ * 
+ * UPDATES FOR 1.2.4:  Enabled "copy to clipboard" setting added.  The user can
+ * no enable and disable copying the generated password to the clipboard.
+ *
+ * This program is Copyright 2011, Jeffrey T. Darlington.
+ * E-mail:  android_support@cryptnos.com
+ * Web:     http://www.cryptnos.com/
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See theGNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA.
+*/
+package com.gpfcomics.android.cryptnos;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.text.ClipboardManager;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * The Generate Existing Passphrase activity. Assuming that a set of site
+ * parameters have already been saved to the database, this activity provides
+ * an interface to generate the resulting password without altering the
+ * parameters themselves.  In other words, this is a read-and-generate only
+ * view, allowing the user to generate the password for a given site without
+ * worrying about fat-fingering something and screwing up the site's settings. 
+ * @author Jeffrey T. Darlington
+ * @version 1.2.4
+ * @since 1.0
+ */
+public class GenerateExistingActivity extends Activity {
+
+	/** A constant indicating the Help option menu item. */
+	public static final int OPTMENU_HELP = Menu.FIRST;
+
+	/** The EditText view containing the user's passphrase. */
+	private EditText txtPassphrase = null;
+	/** The TextView that will display the site name */
+	private TextView lblSiteName = null;
+	/** The TextView that will display the other parameters in a read-only
+	 *  fashion. */
+	private TextView lblOtherParams = null;
+	/** The EditText view that will eventually contain the final password. */
+	private EditText txtOutput = null;
+	/** The Button that actually generates the password and saves it to
+	 *  the database. */
+	private Button btnGenerate = null;
+
+	/** A reference to our top-level application */
+	private CryptnosApplication theApp = null;
+	/** Our database adapter for manipulating the database. */
+	private ParamsDbAdapter dbHelper = null;
+	/** Since this activity only deals with one set of parameters at a time,
+	 *  this member will hold the parameters to make working with them
+	 *  easier. */
+	private SiteParameters params = null;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+        // The usual GUI setup stuff:
+    	super.onCreate(savedInstanceState);
+        setContentView(R.layout.gen_exist_layout);
+        // Open the database:
+        // Get a reference to the top-level application, as well as the
+        // DB helper:
+        theApp = (CryptnosApplication)getApplication();
+        dbHelper = theApp.getDBHelper();
+        // Get handier references to our various input/output controls:
+        txtPassphrase = (EditText)findViewById(R.id.txtPassphrase);
+        lblSiteName = (TextView)findViewById(R.id.labelSiteName);
+        lblOtherParams = (TextView)findViewById(R.id.labelOtherParams);
+        txtOutput = (EditText)findViewById(R.id.txtOutput);
+        btnGenerate = (Button)findViewById(R.id.btnGenerate);
+        
+        // Asbestos underpants:
+        try {
+        	// Check to see if we currently have a saved state for this activity,
+        	// such as before a screen rotate or before the user slides out a
+        	// physical keyboard.  If such a state exists, we want to restore it:
+	        final ParameterViewState state =
+	        	(ParameterViewState)getLastNonConfigurationInstance();
+	        // We found a previously saved state, so re-populate the form:
+	        if (state != null) {
+	        	params = state.getSiteParameters();
+	        	populateParametersLabel();
+	        	txtPassphrase.setText(state.getMasterPassword());
+	        	txtOutput.setText(state.getGeneratedPassword());
+	        // There was no saved state, so proceed to the next step:
+	        } else {
+	            // This activity doesn't make any sense if we don't get anything
+	            // from the caller, so make sure we get valid input:
+	            Bundle extras = getIntent().getExtras();
+	            if (extras != null)
+	            {
+        			// Try to get the site token from the intent bundle.  If
+        			// get got one, try to load the parameters from the
+        			// database:
+        			String site =
+        				extras.getString(ParamsDbAdapter.DBFIELD_SITE);
+        			if (site != null)
+        			{
+        				Cursor c = dbHelper.fetchRecord(site);
+        				startManagingCursor(c);
+        				// Got our record:
+        				if (c.getCount() == 1)
+        				{
+            				// Get the parameters.  Note that we need both the
+            				// site key and the full encrypted string to get
+            				// everything back out.
+            				params = new SiteParameters(theApp,
+            						c.getString(1),
+            						c.getString(2));
+            				// Populate the other parameters label with the
+            				// parameters other than the passphrase.  All of this
+            				// is essentially read-only info in this case, so
+            				// it's purely informative.
+            				populateParametersLabel();
+        				}
+        				// If we didn't get exactly one row, throw an error:
+        				else
+        				{
+        					Toast.makeText(this, R.string.error_bad_restore,
+        						Toast.LENGTH_LONG).show();
+        					finish();
+        				}
+        				c.close();
+        			}
+        			// If the site name was not in the intent bundle, throw
+        			// an error:
+        			else
+        			{
+    					Toast.makeText(this, R.string.error_bad_restore,
+       						Toast.LENGTH_LONG).show();
+    					finish();
+        			}
+	            }
+	            // The bundle was empty, which shouldn't happen:
+	            else
+	            {
+	            	Toast.makeText(this, R.string.error_bad_restore,
+	    				Toast.LENGTH_LONG).show();
+					finish();
+	            }
+	        }
+	        
+	        // Now define what to do if the Generate button is clicked:
+	        btnGenerate.setOnClickListener(new View.OnClickListener() {
+				public void onClick(View v) {
+					// Make sure the passphrase box isn't empty:
+					if (txtPassphrase.getText() == null ||
+						txtPassphrase.getText().length() == 0)
+							Toast.makeText(v.getContext(),
+								R.string.error_edit_bad_password,
+								Toast.LENGTH_LONG).show();
+					// Otherwise, try to generate the password:
+					else
+					{
+						try
+						{
+							// In theory, this part should be easy:
+							String password =
+								params.generatePassword(txtPassphrase.getText().toString());
+							// Display the generated password in the output text
+							// box: 
+							txtOutput.setText(password);
+							// If the user chose to copy the password to the clipboard,
+							// go ahead and copy it now:
+							if (theApp.copyPasswordsToClipboard()) {
+								ClipboardManager clippy =
+									(ClipboardManager)getSystemService(CLIPBOARD_SERVICE);
+								clippy.setText(password);
+								// We'll assume both of those tasks were successful:
+								Toast.makeText(v.getContext(), R.string.edit_gen_success,
+										Toast.LENGTH_LONG).show();
+							// Otherwise, just confirm to the user that the password
+							// was generated:
+							} else Toast.makeText(v.getContext(),
+									R.string.edit_gen_success_no_copy,
+									Toast.LENGTH_LONG).show();
+						}
+						catch (Exception e)
+						{
+							Toast.makeText(v.getContext(), R.string.error_bad_generate,
+									Toast.LENGTH_LONG).show();
+						}
+					}
+				}
+	        });
+
+	    // Something blew up alone the way:
+        } catch (Exception e) {
+			Toast.makeText(this, R.string.error_bad_restore,
+					Toast.LENGTH_LONG).show();
+			finish();
+        }
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+    	// Add the "Help" menu item:
+    	menu.add(0, OPTMENU_HELP, Menu.NONE,
+        	R.string.optmenu_help).setIcon(android.R.drawable.ic_menu_help);
+    	return true;
+    }
+    
+	@Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	switch (item.getItemId()) {
+	    	// If the Help item is selected, open up the "Working with
+    		// existing parameters" help:
+	    	case OPTMENU_HELP:
+	        	Intent i = new Intent(this, HelpActivity.class);
+	        	i.putExtra("helptext", R.string.help_text_existing);
+	        	startActivity(i);
+	    		return true;
+    	}
+    	return false;
+    }
+
+	public Object onRetainNonConfigurationInstance() {
+		// This gets called primarily if the user rotates the screen or slides out
+		// the physical keyboard.  In these cases, we don't want to wipe out the
+		// user's data from the form, so we'll need to preserve it.  Stuff the
+		// site parameters, the user's master password, and the generated password
+		// into a temporary object and hold onto that until we return from the
+		// rebuilding process.
+		final ParameterViewState state = new ParameterViewState(params,
+				txtPassphrase.getText().toString(),
+				txtOutput.getText().toString());
+		return state;
+	}
+
+	/**
+	 * Populate the parameter labels using the data in the SiteParameters object.
+	 * This has been pulled out into its own function since it technically gets
+	 * called multiple times.
+	 */
+	private void populateParametersLabel() {
+		// As a safety measure, make sure the parameters object exists before
+		// we start reading from it:
+		if (params != null) {
+			lblSiteName.setText(params.getSite());
+			String nl = System.getProperty("line.separator");
+			lblOtherParams.setText(
+				getResources().getString(R.string.gen_exist_hash_prompt) + " " +
+				params.getHash() + nl +
+				getResources().getString(R.string.gen_exist_iterations_prompt) + " " +
+				Integer.toString(params.getIterations()) + nl +
+				getResources().getString(R.string.gen_exist_chartypes_prompt) + " " +
+				getResources().getStringArray(R.array.charTypeList)[params.getCharTypes()] + nl +
+				getResources().getString(R.string.gen_exist_charlimit_prompt) + " " +
+				(params.getCharLimit() == 0 ?
+					getResources().getString(R.string.gen_exist_charlimit_none) : 
+					Integer.toString(params.getCharLimit())));
+		}
+	}
+}
